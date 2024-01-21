773aa97c5cf064b09e955e99924994d592c40c19
Simmigon Flagg <simmigon.flagg@gmail.com>
2024-01-20 21:12:37 -0500

    Added missing dependencies

--- app/components/EditTodoForm.js
+++ app/components/EditTodoForm.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react'
 
-export const EditTodoForm = ({editTodo, task}) => {
-    const [value, setValue] = useState(task.task);
+import React, { useState } from 'react'
 
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        // edit todo
-        editTodo(value, task.id);
-      };
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    // edit todo
+    editTodo(value, task.id);
+  };
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
-    <button type="submit" className='todo-btn'>Add Task</button>
-  </form>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+      <button type="submit" className='todo-btn'>Add Task</button>
+    </form>
   )
 }
