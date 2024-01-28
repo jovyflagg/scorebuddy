158defa4d93087b1caf55d47de5b69097102c5e6
Jovy Cortez <jovycortez11@gmail.com>
2024-01-27 23:56:40 -0500

    Added Players

--- src/app/games/[id]/page.jsx
+++ src/app/games/[id]/page.jsx
@@ -1,31 +1,67 @@
-"use client"
+"use client";
 import React, { useContext } from "react";
 // import constants from "../../utils/constants";
 import constants from "../../../utils/constants";
 import styles from "./gamesPage.module.css";
-import GameContext from '@/context/GameContext';
+import GameContext from "@/context/GameContext";
+import {
+  Avatar,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 export default function Games({ params }) {
-  const { games } = useContext(GameContext)
-  const playersGame = games.find(game => {
-    {game.id === params.id}
-  })
-  const selectedGame = games.find(game => game.id === params.id);
+  const { games } = useContext(GameContext);
+  const playersGame = games.find((game) => {
+    {
+      game.id === params.id;
+    }
+  });
+  const selectedGame = games.find((game) => game.id === params.id);
 
   if (selectedGame) {
-      console.log(selectedGame.name);
+    console.log(selectedGame.name);
   } else {
-      console.log('No game found with the specified id.');
+    console.log("No game found with the specified id.");
   }
   return (
     <div className={styles.container}>
       <title>{constants.pages.games}</title>
       <div className={styles.about}>{constants.pages.games}</div>
       <h3>{params ? params.id : null}</h3>
-      {JSON.stringify(playersGame)}
-      {selectedGame.name }
-   
-   
+      {/* {JSON.stringify(selectedGame)} */}
+      {selectedGame.name}
+      <Box sx={{ width: "100%" }}>
+        <nav aria-label="secondary mailbox folders">
+          <List>
+            {selectedGame.players.map((player, index) => {
+              const initial = player.name.slice(0, 2).toUpperCase();
+              return (
+                <div>
+                  <ListItem disablePadding key={player.id}>
+                    <ListItemButton
+                      // onClick={() => router.push(`/games/${player.id}`)}
+                    >
+                      <ListItemIcon>
+                        <Avatar>{initial}</Avatar>
+                      </ListItemIcon>
+                      <ListItemText primary={player.name} />
+                    </ListItemButton>
+                      <ListItemText primary={player.score} />
+                  </ListItem>
+                  {index !== selectedGame.length - 1 && <Divider />}
+                </div>
+              );
+            })}
+          </List>
+        </nav>
+      </Box>
+
       <div className={styles.aboutContainer}>
         <div className={styles.aboutDesc}></div>
       </div>
--- src/providers/GameContextProvider.jsx
+++ src/providers/GameContextProvider.jsx
@@ -5,11 +5,11 @@ import GameContext from "@/context/GameContext";
 const GameContextProvider = ({ children }) => {
 
     const [games, setGames] = useState([
-        { id: "1", name: "5 Crowns" },
-        { id: "2", name: "Scrabble" },
-        { id: "3", name: "Uno" },
-        { id: "4", name: "Tekken" },
-        { id: "5", name: "Dominion" },
+        { id: "1", name: "5 Crowns", players: [{id: "1", name:"Jovy", score: "0"}, {id: "2", name:"Simmigon", score: "100"}] },
+        { id: "2", name: "Scrabble", players: [{id: "1", name:"Jonah", score: "0"}, {id: "2", name:"Tim", score: "100"}]},
+        { id: "3", name: "Uno", players: [{id: "1", name:"Amanda", score: "0"}, {id: "2", name:"Opin", score: "100"}] },
+        { id: "4", name: "Tekken", players: [{id: "1", name:"Denzel", score: "0"}, {id: "2", name:"Isaiah", score: "100"}] },
+        { id: "5", name: "Dominion", players: [{id: "1", name:"Joselle", score: "0"}, {id: "2", name:"Alan", score: "100"}] },
     ]);
 
     return (
