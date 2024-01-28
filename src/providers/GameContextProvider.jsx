2966636a9a7dbeffb80210388ad333239a8db1a6
Jovy Cortez <jovycortez11@gmail.com>
2024-01-28 00:16:35 -0500

    Nanoid added for games and player id

--- src/providers/GameContextProvider.jsx
+++ src/providers/GameContextProvider.jsx
@@ -1,15 +1,17 @@
 "use client";
 import React, { useState } from 'react'
 import GameContext from "@/context/GameContext";
+import { nanoid } from 'nanoid'
+// const model.id = nanoid()  //=> "V1StGXR8_Z5jdHi6B-myT"
 
 const GameContextProvider = ({ children }) => {
-
+   
     const [games, setGames] = useState([
-        { id: "1", name: "5 Crowns", players: [{id: "1", name:"Jovy", score: "0"}, {id: "2", name:"Simmigon", score: "100"}] },
-        { id: "2", name: "Scrabble", players: [{id: "1", name:"Jonah", score: "0"}, {id: "2", name:"Tim", score: "100"}]},
-        { id: "3", name: "Uno", players: [{id: "1", name:"Amanda", score: "0"}, {id: "2", name:"Opin", score: "100"}] },
-        { id: "4", name: "Tekken", players: [{id: "1", name:"Denzel", score: "0"}, {id: "2", name:"Isaiah", score: "100"}] },
-        { id: "5", name: "Dominion", players: [{id: "1", name:"Joselle", score: "0"}, {id: "2", name:"Alan", score: "100"}] },
+        { id: nanoid(), name: "5 Crowns", players: [{id: nanoid(), name:"Jovy", score: "0"}, {id: nanoid(), name:"Simmigon", score: "100"}] },
+        { id: nanoid(), name: "Scrabble", players: [{id: nanoid(), name:"Jonah", score: "0"}, {id: nanoid(), name:"Tim", score: "100"}]},
+        { id: nanoid(), name: "Uno", players: [{id: nanoid(), name:"Amanda", score: "0"}, {id: nanoid(), name:"Opin", score: "100"}] },
+        { id: nanoid(), name: "Tekken", players: [{id: nanoid(), name:"Denzel", score: "0"}, {id: nanoid(), name:"Isaiah", score: "100"}] },
+        { id: nanoid(), name: "Dominion", players: [{id: nanoid(), name:"Joselle", score: "0"}, {id: nanoid(), name:"Alan", score: "100"}] },
     ]);
 
     return (
